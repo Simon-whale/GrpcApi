52ea762b456d9ed563d297dd76e7cf63ff277a01
Simon-whale <83454878+Simon-whale@users.noreply.github.com>
2021-05-24 22:11:06 +0100

    * .gitignore: Adding initial gRPC WebAPi
    TODO: add multiple service implementations
    
    * GrpcApi.sln:
    * Program.cs:
    * Startup.cs:
    * Program.cs:
    * GrpcApi.csproj:
    * appsettings.json:
    * Calculator.cs:
    * GrpcClient.csproj:
    * Calculator.cs:
    * CalculatorGrpc.cs:
    * CalculatorGrpc.cs:
    * appsettings.Development.json:
    * launchSettings.json:
    * CalculatorServiceImpl.cs:
    * WeatherForecastController.cs:

---
+++ GrpcApi/Services/CalculatorServiceImpl.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Grpc.Core;
+using Sums;
+using static Sums.SumsService;
+
+namespace GrpcApi.Services
+{
+    public class CalculatorServiceImpl : SumsServiceBase
+    {
+        public override Task<SumsResponse> Sums(SumsRequest request, ServerCallContext context)
+        {
+            var result = request.NumbersToAdd.First + request.NumbersToAdd.Second;
+            return Task.FromResult(new SumsResponse() { Result = result });
+        }
+    }
+}
---
+++ GrpcClient/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Grpc.Core;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        const string target = "localhost:5002";
+
+        static void Main(string[] args)
+        {
+            Channel channel = new Channel(target, ChannelCredentials.Insecure);
+            channel.ConnectAsync().ContinueWith((task) =>
+            {
+                if (task.Status == System.Threading.Tasks.TaskStatus.RanToCompletion)
+                    Console.WriteLine("The client connected successfully");
+            });
+
+            var client = new Sums.SumsService.SumsServiceClient(channel);
+            var calc = new Sums.Sums()
+            {
+                First = 5,
+                Second = 8
+            };
+
+            var request = new Sums.SumsRequest() { NumbersToAdd = calc };
+            var response = client.Sums(request);
+
+            Console.WriteLine(response.Result);
+
+            channel.ShutdownAsync().Wait();
+            Console.ReadKey();
+        }
+    }
+}
