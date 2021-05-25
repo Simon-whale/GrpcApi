a5ba80d9a5e51b6478af5efadf36a4aafbbbf3dc
Simon-whale <83454878+Simon-whale@users.noreply.github.com>
2021-05-25 12:04:16 +0100

    * Helloworld.cs: * Helloworld.cs: * HelloworldGrpc.cs: * HelloworldGrpc.cs: * HelloWorldServiceImpl.cs:
    * GrpcApi.sln: Adding second service implementation on the API and
      caller in the client

---
+++ GrpcApi/Services/HelloWorldServiceImpl.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Helloworld;
+using static Helloworld.helloWorldService;
+
+namespace GrpcApi.Services
+{
+    public class HelloWorldServiceImpl : helloWorldServiceBase
+    {
+        public override Task<sayHelloResponse> person(sayHelloRequest request, ServerCallContext context)
+        {
+            var result = $"Hello {request.To.Firstname} {request.To.Surname} how are you?";
+            return Task.FromResult(new sayHelloResponse() { Result = result });
+        }
+    }
+}
